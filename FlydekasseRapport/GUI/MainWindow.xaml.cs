8e05ee7251d4b2d9707db8ed50f9ce4f1aa91e6d
Lucas Kyhl <luca609a@edu.campusvejle.dk>
2021-08-25 15:00:57 +0200

    Added event handlers and functionality to them

--- FlydekasseRapport/GUI/MainWindow.xaml.cs
+++ FlydekasseRapport/GUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BIZ;
 
 namespace GUI
 {
@@ -20,9 +21,33 @@ namespace GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        ClassBIZ BIZ;
         public MainWindow()
         {
             InitializeComponent();
+            BIZ = new ClassBIZ();
+            MainGrid.DataContext = BIZ;
+        }
+
+        private void buttonAddMaterialToList_Click(object sender, RoutedEventArgs e)
+        {
+            if (BIZ.selectedMaterial.materialWeight > 0 && BIZ.selectedMaterial.materialDim > 0) // Make sure thickness is bigger than zero
+            {
+                BIZ.AddMaterialToSelectedList();
+            }
+        }
+
+        private void buttonAddDimensionsToList_Click(object sender, RoutedEventArgs e)
+        {
+            if (BIZ.selectedBox.boxHeight > 0 && BIZ.selectedBox.boxWidth > 0 && BIZ.selectedBox.boxDepth > 0)
+            {
+                BIZ.AddBoxToSelectedList();
+            }
+        }
+
+        private void buttonMakeReportAndShow_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
