164f3206626369f7fefbb879c6575431c5f1dd43
Lucas Kyhl <luca609a@edu.campusvejle.dk>
2021-08-30 14:58:47 +0200

    Added unit testing for addition with 1 test

---
+++ MyCalculator/MyCalculator.UnitTests/CalculatorMinusTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BIZ;
+
+namespace MyCalculator.UnitTests
+{
+    [TestClass]
+    public class CalculatorMinusTests
+    {
+        ClassCalculator CC = new ClassCalculator();
+
+        [TestMethod]
+        public void CanCalculateSubtraction()
+        {
+            // Arrange
+            CC.tal1 = "10";
+            CC.tal2 = "5";
+            double finalRes = 5D;
+
+            // Act
+            double res = CC.CalcResMinus();
+
+            // Assert
+            Assert.AreEqual(finalRes, res);
+        }
+    }
+}
