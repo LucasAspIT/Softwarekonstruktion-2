2291562c46d3f2a9d164904e949e15530ce36dd5
Lucas Kyhl <luca609a@edu.campusvejle.dk>
2021-08-31 09:33:41 +0200

    Added a new [TestMethod], renamed method

--- MyCalculator/MyCalculator.UnitTests/CalculatorMinusTests.cs
+++ MyCalculator/MyCalculator.UnitTests/CalculatorMinusTests.cs
@@ -10,18 +10,33 @@ namespace MyCalculator.UnitTests
         ClassCalculator CC = new ClassCalculator();
 
         [TestMethod]
-        public void CanCalculateSubtraction()
+        public void CanCalculate_Subtraction_CorrectlyCalculatesIntegers()
         {
             // Arrange
             CC.tal1 = "10";
             CC.tal2 = "5";
-            double finalRes = 5D;
+            double finalRes = 5;
 
             // Act
             double res = CC.CalcResMinus();
 
             // Assert
             Assert.AreEqual(finalRes, res);
+        }
+
+        [TestMethod]
+        public void CanCalculate_Subtraction_CorrectlyCalculatesDoubles()
+        {
+            // Arrange
+            CC.tal1 = "33,4";
+            CC.tal2 = "66,9";
+            double finalRes = -33.5D;
+
+            // Act
+            double res = CC.CalcResMinus();
+
+            // Assert
+            Assert.AreEqual(finalRes, res, 0.00001);
         }
     }
 }
