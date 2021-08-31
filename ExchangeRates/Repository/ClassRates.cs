c6cce517c8ffa5beadd5fea669245fe331eea370
Lucas Kyhl <luca609a@edu.campusvejle.dk>
2021-08-31 11:59:05 +0200

    Initial push, non-function atm

---
+++ ExchangeRates/Repository/ClassRates.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    class ClassRates : ClassNotify
+    {
+        public ClassRates()
+        {
+            key = "";
+            value = 0;
+        }
+
+        private string _key;
+        private double _value;
+
+        public string key
+        {
+            get { return _key; }
+            set
+            {
+                if (_key != value)
+                {
+                    _key = value;
+                }
+                Notify("key");
+            }
+        }
+
+        public double value
+        {
+            get { return _value; }
+            set
+            {
+                if (_value != value)
+                {
+                    _value = value;
+                }
+                Notify("value");
+            }
+        }
+
+    }
+}
