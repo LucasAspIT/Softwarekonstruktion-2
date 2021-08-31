604851043d0556fa0a71b2cbd9c5aa6db2cebe39
Lucas Kyhl <luca609a@edu.campusvejle.dk>
2021-08-31 09:32:36 +0200

    Added new unit test with two tests

---
+++ MyCalculator/MyCalculator.UnitTests/CalculatorMultiplicationTests.cs
@@ -0,0 +1,42 @@
+﻿using BIZ;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MyCalculator.UnitTests
+{
+    [TestClass]
+    public class CalculatorMultiplicationTests
+    {
+        ClassCalculator CC = new ClassCalculator();
+
+        [TestMethod]
+        public void CanCalculate_Multiplication_CorrectlyCalculatesIntegers()
+        {
+            // Arrange
+            CC.tal1 = "27";
+            CC.tal2 = "88";
+            double finalRes = 2376;
+
+            // Act
+            double res = CC.CalcResGange();
+
+            // Assert
+            Assert.AreEqual(finalRes, res);
+        }
+
+        [TestMethod]
+        public void CanCalculate_Multiplication_CorrectlyCalculatesDoubles()
+        {
+            // Arrange
+            CC.tal1 = "22,1";
+            CC.tal2 = "45,6";
+            double finalRes = 1007.76D;
+
+            // Act
+            double res = CC.CalcResGange();
+
+            // Assert
+            Assert.AreEqual(finalRes, res, 0.00001);
+        }
+    }
+}
