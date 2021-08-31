b09b2b2b3b09ed7711ce86b3236fcb79d17b60c7
Lucas Kyhl <luca609a@edu.campusvejle.dk>
2021-08-31 11:59:38 +0200

    Added some properties and initialised, non-functioning atm

--- ExchangeRates/Repository/ExchangeRates.cs
+++ ExchangeRates/Repository/ExchangeRates.cs
@@ -6,12 +6,89 @@ using System.Threading.Tasks;
 
 namespace Repository
 {
-    public class ExchangeRates
+    public class ExchangeRates : ClassNotify
     {
 
         public ExchangeRates()
         {
-
+            disclaimer = "";
+            license = "";
+            timestamp = 0;
+            Base = "";
+            rates = new List<ClassRates>();
         }
+
+        private string _disclaimer;
+        private string _license;
+        private int _timestamp;
+        private string _Base;
+        private List<ClassRates> _rates;
+
+        public string disclaimer
+        {
+            get { return _disclaimer; }
+            set
+            {
+                if (_disclaimer != value)
+                {
+                    _disclaimer = value;
+                }
+                Notify("disclaimer");
+            }
+        }
+
+        public string license
+        {
+            get { return _license; }
+            set
+            {
+                if (_license != value)
+                {
+                    _license = value;
+                }
+                Notify("license");
+            }
+        }
+
+        public int timestamp
+        {
+            get { return _timestamp; }
+            set
+            {
+                if (_timestamp != value)
+                {
+                    _timestamp = value;
+                }
+                Notify("timestamp");
+            }
+        }
+
+        public string Base
+        {
+            get { return _Base; }
+            set
+            {
+                if (_Base != value)
+                {
+                    _Base = value;
+                }
+                Notify("Base");
+            }
+        }
+
+        public List<ClassRates> rates
+        {
+            get { return _rates; }
+            set
+            {
+                if (_rates != value)
+                {
+                    _rates = value;
+                }
+                Notify("rates");
+            }
+        }
+
+
     }
 }
