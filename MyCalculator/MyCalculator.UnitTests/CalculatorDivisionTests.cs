b4428a537188db814595c4481b3bc4b115fa83bb
Lucas Kyhl <luca609a@edu.campusvejle.dk>
2021-08-31 09:32:17 +0200

    Added new unit test with two tests

---
+++ MyCalculator/MyCalculator.UnitTests/CalculatorDivisionTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BIZ;
+
+namespace MyCalculator.UnitTests
+{
+    [TestClass]
+    public class CalculatorDivisionTests
+    {
+        ClassCalculator CC = new ClassCalculator();
+
+        [TestMethod]
+        public void CanCalculate_Division_CorrectlyCalculatesDoubles()
+        {
+            // Arrange
+            CC.tal1 = "22,1";
+            CC.tal2 = "45,6";
+            double finalRes = 0.48464D;
+
+            // Act
+            double res = CC.CalcResDiv();
+
+            // Assert
+            Assert.AreEqual(finalRes, res, 0.00001);
+        }
+
+        [TestMethod]
+        public void CanCalculate_DivisionByZero_CorrectlyDefaultToZero()
+        {
+            // Arrange
+            CC.tal1 = "35";
+            CC.tal2 = "0";
+            double finalRes = 0;
+
+            // Act
+            double res = CC.CalcResDiv();
+
+            // Assert
+            Assert.AreEqual(finalRes, res);
+        }
+    }
+}
