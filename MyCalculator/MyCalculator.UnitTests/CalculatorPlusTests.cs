98bf0d9492f151612c51ce9aabec3ebc6e425722
Lucas Kyhl <luca609a@edu.campusvejle.dk>
2021-08-31 09:34:00 +0200

    Added new [TestMethod], renamed method

--- MyCalculator/MyCalculator.UnitTests/CalculatorPlusTests.cs
+++ MyCalculator/MyCalculator.UnitTests/CalculatorPlusTests.cs
@@ -10,18 +10,33 @@ namespace MyCalculator.UnitTests
         ClassCalculator CC = new ClassCalculator();
 
         [TestMethod]
-        public void CanCalculateAddition()
+        public void CanCalculate_Addition_ReturnsCalculatedIntegers()
         {
             // Arrange
             CC.tal1 = "55";
             CC.tal2 = "45";
-            double finalRes = 100D;
+            double finalRes = 100;
 
             // Act
             double res = CC.CalcResPlus();
 
             // Assert
             Assert.AreEqual(finalRes, res);
+        }
+
+        [TestMethod]
+        public void CanCalculate_Addition_CorrectlyCalculatesDoubles()
+        {
+            // Arrange
+            CC.tal1 = "33,4";
+            CC.tal2 = "66,9";
+            double finalRes = 100.3D;
+
+            // Act
+            double res = CC.CalcResPlus();
+
+            // Assert
+            Assert.AreEqual(finalRes, res, 0.00001);
         }
     }
 }
