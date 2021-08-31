3c0a5cd1ec0dfaaee6fdd4eab7643230aba31ad3
Lucas Kyhl <luca609a@edu.campusvejle.dk>
2021-08-31 11:22:27 +0200

    Add an [ExpectedException] testing for (OverflowException)

--- MyCalculator/MyCalculator.UnitTests/CalculatorMultiplicationTests.cs
+++ MyCalculator/MyCalculator.UnitTests/CalculatorMultiplicationTests.cs
@@ -15,7 +15,7 @@ namespace MyCalculator.UnitTests
             // Arrange
             CC.tal1 = "27";
             CC.tal2 = "88";
-            double finalRes = 2376;
+            double finalRes = 2376D;
 
             // Act
             double res = CC.CalcResGange();
@@ -36,7 +36,24 @@ namespace MyCalculator.UnitTests
             double res = CC.CalcResGange();
 
             // Assert
-            Assert.AreEqual(finalRes, res, 0.00001);
+            Assert.AreEqual(finalRes, res, 0.00001D);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException),
+         "Overflow exception expected.")]
+        public void CanHandle_Multiplication_ThrownOverflowException()
+        {
+            // Arrange
+            CC.tal1 = "9999999999";
+            CC.tal2 = "9999999999";
+            // double finalRes = 1007.76D;
+
+            // Act
+            int res = Convert.ToInt32(CC.CalcResGange());
+
+            // Assert
+            // Assert.AreEqual(finalRes, res, 0.00001D);
         }
     }
 }
