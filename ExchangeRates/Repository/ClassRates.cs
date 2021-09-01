936b4db961e9f2e42c1976ddd40d6f705842538b
LucasAspIT <luca609a@edu.campusvejle.dk>
2021-09-01 15:07:30 +0200

    Added lots of properties used in the GUI

--- ExchangeRates/Repository/ClassRates.cs
+++ ExchangeRates/Repository/ClassRates.cs
@@ -6,16 +6,116 @@ using System.Threading.Tasks;
 
 namespace Repository
 {
-    class ClassRates : ClassNotify
+    public class ClassRates : ClassNotify
     {
         public ClassRates()
         {
+            disclaimer = "";
+            license = "";
+            strDateTime = "";
+            timestamp = 0;
+            Base = "";
             key = "";
-            value = 0;
+            value = 0D;
+            _DKK = 0D;
+            _rateDK = 0D;
+        }
+
+        public ClassRates(ExchangeRates rates, string inKey, double inValue)
+        {
+            _DKK = rates.rates["DKK"];
+            _rateDK = 0D;
+            disclaimer = rates.disclaimer;
+            license = rates.license;
+            strDateTime = "";
+            timestamp = rates.timestamp;
+            Base = rates.Base;
+            key = inKey;
+            value = inValue;
         }
 
         private string _key;
         private double _value;
+        private string _Base;
+        private int _timestamp;
+        private string _license;
+        private string _disclaimer;
+        private string _strDateTime;
+        private double _rateDK;
+
+        private double _DKK;
+
+
+        public double rateDK
+        {
+            get { return _rateDK; }
+            set
+            {
+                if (_rateDK != value)
+                {
+                    _rateDK = value;
+                }
+                Notify("rateDK");
+            }
+        }
+
+
+        public string disclaimer
+        {
+            get { return _disclaimer; }
+            set
+            {
+                if (_disclaimer != value)
+                {
+                    _disclaimer = value;
+                }
+                Notify("disclaimer");
+            }
+        }
+
+
+        public string license
+        {
+            get { return _license; }
+            set
+            {
+                if (_license != value)
+                {
+                    _license = value;
+                }
+                Notify("license");
+            }
+        }
+
+
+        public int timestamp
+        {
+            get { return _timestamp; }
+            set
+            {
+                if (_timestamp != value)
+                {
+                    _timestamp = value;
+                    MakeTimeString();
+                }
+                Notify("timestamp");
+            }
+        }
+
+
+        public string Base
+        {
+            get { return _Base; }
+            set
+            {
+                if (_Base != value)
+                {
+                    _Base = value;
+                }
+                Notify("Base");
+            }
+        }
+
 
         public string key
         {
@@ -38,10 +138,33 @@ namespace Repository
                 if (_value != value)
                 {
                     _value = value;
+                    CalculateRateInDKK();
                 }
                 Notify("value");
             }
         }
 
+        public string strDateTime
+        {
+            get { return _strDateTime; }
+            set
+            {
+                if (_strDateTime != value)
+                {
+                    _strDateTime = value;
+                }
+                Notify("strDateTime");
+            }
+        }
+
+        private void MakeTimeString()
+        {
+            strDateTime = DateTimeOffset.FromUnixTimeSeconds(timestamp).ToString("dd-MM-yyyy HH:mm:ss") + " (GMT+0)"; // GMT because openexchangerates is based in the UK
+        }
+
+        private void CalculateRateInDKK()
+        {
+            rateDK = 1 / value * _DKK;
+        }
     }
 }
