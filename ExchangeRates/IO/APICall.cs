117ca678644a401a9fc993e75b1cf6c6c1fb887d
LucasAspIT <luca609a@edu.campusvejle.dk>
2021-09-01 15:05:50 +0200

    Aded method to call Web APIs

--- ExchangeRates/IO/APICall.cs
+++ ExchangeRates/IO/APICall.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +13,23 @@ namespace IO
         public APICall()
         {
 
+        }
+
+        protected async Task<string> GetDataFromWebAPIAsync(string URL)
+        {
+            var content = new MemoryStream();
+
+            var webReq = (HttpWebRequest)WebRequest.Create(URL);
+
+            using (WebResponse response = await webReq.GetResponseAsync())
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    await responseStream.CopyToAsync(content);
+                }
+            }
+
+            return Encoding.UTF8.GetString(content.ToArray());
         }
     }
 }
