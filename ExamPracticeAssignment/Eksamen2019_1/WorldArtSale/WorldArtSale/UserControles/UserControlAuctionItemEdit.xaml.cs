4313dc399af2e09b5574e60c764095916e12b800
Lucas Kyhl <luca609a@edu.campusvejle.dk>
2021-09-06 14:59:59 +0200

    Added new UserControl for adding auction items

---
+++ ExamPracticeAssignment/Eksamen2019_1/WorldArtSale/WorldArtSale/UserControles/UserControlAuctionItemEdit.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BIZ;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for UserControlAuctionItemEdit.xaml
+    /// </summary>
+    public partial class UserControlAuctionItemEdit : UserControl
+    {
+        ClassBIZ BIZ;
+        Grid motherGrid;
+
+        public UserControlAuctionItemEdit(ClassBIZ inBIZ, Grid inGrid)
+        {
+            InitializeComponent();
+            BIZ = inBIZ;
+            motherGrid = inGrid;
+        }
+
+        private void AddNewArtToDB_Click(object sender, RoutedEventArgs e)
+        {
+            motherGrid.Children.Remove(this);
+        }
+    }
+}
