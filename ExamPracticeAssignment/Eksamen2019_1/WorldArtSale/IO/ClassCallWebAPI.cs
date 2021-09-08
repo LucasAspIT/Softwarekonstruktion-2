47f8911f960d8dd55f5b50a4641899a70ab5cf31
LucasAspIT <luca609a@edu.campusvejle.dk>
2021-09-08 15:10:56 +0200

    Finished method

--- ExamPracticeAssignment/Eksamen2019_1/WorldArtSale/IO/ClassCallWebAPI.cs
+++ ExamPracticeAssignment/Eksamen2019_1/WorldArtSale/IO/ClassCallWebAPI.cs
@@ -1,19 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using System;
+using System.IO;
+using System.Net;
 
 namespace IO
 {
-    class ClassCallWebAPI
+    public class ClassCallWebAPI
     {
 
-        public async Task<byte[]> GetURLContentsAsync(string url)
+        public async Task<string> GetURLContentsAsync(string url)
         {
-            byte[] res = new byte[5];
+            var content = new MemoryStream();
 
-            return res;
+            // Initialize an HttpWebRequest for the current URL.
+            var webReq = (HttpWebRequest)WebRequest.Create(url);
+
+            try
+            {
+                // Send the request to the Internet resource and wait for the response.
+                // Note: You can't use HttpWebRequest.GetResponse in a Windows Store app.
+                using (WebResponse response = await webReq.GetResponseAsync())
+                {
+                    // Get the data stream that is associated with the specified URL.
+                    using (Stream responseStream = response.GetResponseStream())
+                    {
+                        // Read the bytes in responseStream and copy them to content.
+                        await responseStream.CopyToAsync(content);
+                        // The downloaded resource ends up in the variable named content.
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            // Return the result as a byte array.
+            return System.Text.Encoding.UTF8.GetString(content.ToArray());
         }
     }
 }
